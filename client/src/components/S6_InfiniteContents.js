ce3d2bddebf7295166a44552784473d6708b2249
DESKTOP-D31VJUG\DuyDK <khanhduyapt@gmail.com>
2020-11-27 17:27:36 +0700

    client

--- client/src/components/S6_InfiniteContents.js
+++ client/src/components/S6_InfiniteContents.js
@@ -1,10 +1,56 @@
-import React from "react";
+import React, { useState, useRef, useCallback } from "react";
+import useBookSearch from "./commons/useBookSearch";
 import "./S6_InfiniteContents.css";
 
 function S6_InfiniteContents() {
+  const [query, setQuery] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const { books, hasMore, loading, error } = useBookSearch(query, pageNumber);
+
+  const observer = useRef();
+  const lastBookElementRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
+
+  function handleSearch(e) {
+    setQuery(e.target.value);
+    setPageNumber(1);
+  }
+
+  const openInNewTab = (url) => {
+    const newWindow = window.open(url, "_blank", "noopener,noreferrer");
+    if (newWindow) newWindow.opener = null;
+  };
+  //onClick={() => {openInNewTab('https://stackoverflow.com')}}
+
   return (
     <div className="infinite">
       <h1>S6_InfiniteContents</h1>
+      <input type="text" value={query} onChange={handleSearch}></input>
+      {books.map((book, index) => {
+        if (books.length === index + 1) {
+          return (
+            <div ref={lastBookElementRef} key={book}>
+              {book}
+            </div>
+          );
+        } else {
+          return <div key={book}>{book}</div>;
+        }
+      })}
+      <div>{loading && "Loading..."}</div>
+      <div>{error && "Error"}</div>
     </div>
   );
 }
